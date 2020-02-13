86ab2a4f60b9f43cfff591c2e270f4f9de1de443
garyfm <garyfmullen@gmail.com>
2020-02-13 23:15:21 +0000

    Extracted Text

--- met_scrap.py
+++ met_scrap.py
@@ -7,5 +7,12 @@ req = requests.get("https://www.met.ie/weather-forecast/cork-city#forecasts")
 print("Return code: ", req.status_code)
 
 if (req.status_code == 200):
-    html = bs4(req.text, "lxml")
-    print (html.title)
+    met_soup = bs4(req.text, "lxml")
+    print ("Location: ", met_soup.title.string)
+    met_text = met_soup.get_text()
+    met_text = met_text.split(" ")
+    print (met_text)
+
+    cork = [x for x in met_text if x == "Temperature"]
+    print(cork)
+    
