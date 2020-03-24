e75c8400305ddd73d9a8f1890b595f53565b64fe
garyfm <garyfmullen@gmail.com>
2020-03-24 21:49:16 +0000

    removed scrapped data file

--- met_scrap.py
+++ met_scrap.py
@@ -13,23 +13,23 @@ CORK_LONG = -8.468399
 
 lng = CORK_LONG 
 lat = CORK_LAT
-MET_API_URL = "http://metwdb-openaccess.ichec.ie/metno-wdb2ts/locationforecast?"
+MET_API_URL = "http://metwdb-openaccess.ichec.ie/metno-wdb2ts/locationforecast"
 
 payload = {'lat':lat, 'long':lng}
 
 def get_met_data(api_url):
-    req = requests.get(MET_API_URL, params=payload) 
+    req = requests.get(api_url, params=payload) 
     if (req.status_code == 200):
         print(GREEN + "API Request Success: " + str(req.status_code) + ENDC)
         return req.text
     else:
         print(RED + "API Request Fail: " + str(req.status_code) + ENDC)
-
+        return req.status_code
     return
 
 def main():
    met_data = get_met_data(MET_API_URL) 
-   print(met_data)
+   #print(met_data)
 
 if __name__ == "__main__":
     main()
