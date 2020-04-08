d22fadbad5766a64e327fe1b324eb959be41393d
Gary Mullen <garyfmullen@gmail.com>
2020-04-08 14:33:43 -0700

    Basic Graph of Temp Vs Time

--- met_data.py
+++ met_data.py
@@ -4,6 +4,10 @@ import requests
 import os.path
 import xmltodict
 import json
+import datetime as dt
+import numpy as np
+from matplotlib import pyplot as plt
+from matplotlib import dates
 
 # Colors
 RED = '\033[91m'
@@ -54,15 +58,15 @@ def get_met_data(api_url, file_path):
 def parse_met_data(xml_data):
     met_data_xml = xmltodict.parse(xml_data)
     temperature, humidity, rain = [], [], []
-    time, time_point = [], [0,0]
+    time = []
     i, j, k = 0, 0, 0
     
     # Index over all data points
     for data_point in met_data_xml['weatherdata']['product']['time']:
         # Get time points time[[from, to], [from, to], ...]
-        time_point[0] = data_point['@from']
-        time_point[1] = data_point['@to']
-        time.append(time_point)
+        #time[0] = data_point['@from']
+        #time_point[1] = data_point['@to']
+        time.append([data_point['@from'], data_point['@to']])
         print(str(i) + ". from: " +  time[i][0] + " to: " + time[i][1]) 
 
         # Even  = temp and humid, Odd = Rain data points
@@ -81,15 +85,38 @@ def parse_met_data(xml_data):
 
         # Index over all data points 
         i += 1
+    return time, temperature, humidity, rain
+
+def plot_data(time, temp, humid, rain):
+    temp_series, time_series = [], []
+    i = 0
+
+    for time_point in time:
+        if ((i % 2) == 0):
+            time_series.append(time_point[0])
+        i+=1
+    for value in temp:
+        temp_series.append(value['@value'])
+    
+    plt.plot_date(time_series,  temp_series)
+    #plt.xticks(np.arange(min(time_series), max(time_series)+1), 5)
+#    temp_int = np.arange(min(temp_series), max(temp_series)+1)
+    plt.xlabel("Time")
+    plt.ylabel("Temperature(C)")
+    plt.title("Cork Temperature")
+    plt.show()
+
     return
 
 def main():
+    time, temp, humid, rain = [], [], [], [] 
     met_data_xml = get_met_data(MET_API_URL, MET_DATA_FILE)
     if (met_data_xml == ''):
         print(RED + "Failed to get Met Data" + ENDC)
         return None
 
-    parse_met_data(met_data_xml)
+    time, temp, humid, rain = parse_met_data(met_data_xml)
+    plot_data(time, temp, humid, rain)
     return
 
 if __name__ == "__main__":
